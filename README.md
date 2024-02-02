657b6810a937b3deea1ab6a98d3f3c574a35bbaa
yinyin13 <yinyin13@uw.edu>
2024-02-01 16:57:20 -0800

    Update app.py and README.md

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # techin510_lab3
+## Overview
+This is a vocabulary web app that allows users to manage and track their vocabulary learning progress. Users can add new words, update their progress, and filter/search for specific entries.
+
+## How to run
+To run the application **locally**, please follow the steps below:
+1. Make sure that Python is installed on your system
+2. Clone the repo:
+`git clone https://github.com/yinyin13/techin510_lab3.git`
+3. Open project directory:
+`cd <where the project is cloned>`
+4. Install dependencies:
+`pip install -r requirements.txt`
+5. Run the app on local host:
+`streamlit run app.py`
+
+To access the web app directly online, go to [Word Bank 🔡](https://yinyin13-todo.azurewebsites.net)
+
+## Lessons Learned
+- I'm now more familiar with using SQL clauses
+- I learned how to create interactive UIs using Streamlit
+- I learned the basics of CSS and HTML commands
+
+## Future Improvements for the App
+- Add a feature that allows users to practice using the vocabulary word
+- Change the `update_progress` logic so that users can directly update their learning progress without filling the form again
+- Add a calendar feature for users to better track their learning progress
+- Improve overall UI and usability
--- app.py
+++ app.py
@@ -15,7 +15,7 @@ class WordBank(BaseModel):
     POS: str
     definition: str
     progress: ProgressValue = Field(
-        ..., description="Allows multiple items from a set."
+        ...
     )
 
 con = sqlite3.connect("wordbank.sqlite", isolation_level=None)
@@ -33,6 +33,7 @@ cur.execute(
     """
 )
 
+# Update progress only
 def update_progress(progress, id):
     cur.execute(
         """
@@ -41,18 +42,37 @@ def update_progress(progress, id):
         (progress, id),
     )
 
+# Update entire entry
+def update_entry(data, existing_word_id):
+    cur.execute(
+        """
+        UPDATE word_list SET POS = ?, definition = ?, progress = ? WHERE id = ?
+        """,
+        (data.POS, data.definition, data.progress, existing_word_id),
+    )
+
+# Delete an entry
+def delete_entry(id):
+    cur.execute(
+        """
+        DELETE FROM word_list WHERE id = ?;
+        """,
+        (id,),
+    )
+
 def insert_word(data):
     existing_word = cur.execute(
         """
-        SELECT id, progress FROM word_list WHERE word = ?
+        SELECT id FROM word_list WHERE word = ?
         """,
         (data.word,),
     ).fetchone()
 
     if existing_word:
-        # Word already exists, update progress
-        update_progress(data.progress, existing_word[0])
-        st.info(f"Progress for **{data.word}** updated! New progress: **{data.progress.value}**", icon = "✨")
+        # Word already exists, update the entire entry
+        update_entry(data, existing_word[0])
+        st.info(f"Word **{data.word}** updated!", icon="✨")
+
     else:
         # Word doesn't exist, insert a new record
         cur.execute(
@@ -61,58 +81,113 @@ def insert_word(data):
             """,
             (data.word, data.POS, data.definition, data.progress),
         )
+        st.success(f"New entry for **{data.word}** added!", icon="✨")
+
+        # Display only the entry that has just been entered
+        data = cur.execute(
+            """
+            SELECT * FROM word_list WHERE id = LAST_INSERT_ROWID()
+            """
+        ).fetchall()
+
+        cols = st.columns(5)
+        cols[0].write("")
+        cols[1].write("Progress")
+        cols[2].write("Word")
+        cols[3].write("POS")
+        cols[4].write("Definition")
+
+        def get_progress_color(progress):
+            if progress == "unfamiliar":
+                return "#F76F65"
+            elif progress == "recognized":
+                return "#F3A738"
+            elif progress == "understood":
+                return "#FDF984"
+            elif progress == "mastered":
+                return "#94C747"
+            else:
+                return "black"
+
+        for row in data:
+            progress_color = get_progress_color(row[4])
+            cols = st.columns(5)
+
+            delete_button_id = f"delete_button_{row[0]}.1"
+            if cols[0].button("Delete", key=delete_button_id):
+                # Button clicked, delete the entry
+                delete_entry(row[0])
+                st.rerun()
+
+            cols[1].write(f'<div style="display: inline-block; padding: 6px 10px; background-color:{progress_color}; border-radius: 10px;">{row[4]}</div>', unsafe_allow_html=True)
+            cols[2].write(row[1])
+            cols[3].write(row[2])
+            cols[4].write(row[3])
 
 def main():
     st.title("Word Bank 🔡")
-    data = sp.pydantic_form(key="word_form", model=WordBank)
-    if data:
-        insert_word(data)
 
-    data = cur.execute(
-        """
-        SELECT * FROM word_list
-        """
-    ).fetchall()
+    # Tabs for form and list
+    tabs = st.tabs(["Form", "List"])
 
-    # HINT: how to implement a Edit button?
-    # if st.query_params.get('id') == "123":
-    #     st.write("Hello 123")
-    #     st.markdown(
-    #         f'<a target="_self" href="/" style="display: inline-block; padding: 6px 10px; background-color: #4CAF50; color: white; text-align: center; text-decoration: none; font-size: 12px; border-radius: 4px;">Back</a>',
-    #         unsafe_allow_html=True,
-    #     )
-    #     return
-
-    cols = st.columns(4)
-    cols[0].write("Progress")
-    cols[1].write("Word")
-    cols[2].write("POS")
-    cols[3].write("Definition")
-
-    def get_progress_color(progress):
-        if progress == "unfamiliar":
-            return "#F76F65"
-        elif progress == "recognized":
-            return "#F3A738"
-        elif progress == "understood":
-            return "#FDF984"
-        elif progress == "mastered":
-            return "#94C747"
-        else:
-            return "black"
-    
-    for row in data:
-        progress_color = get_progress_color(row[4])
-        cols = st.columns(4)
-
-        cols[0].write(f'<div style="display: inline-block; padding: 6px 10px; background-color:{progress_color}; border-radius: 10px;">{row[4]}</div>', unsafe_allow_html=True)
-        cols[1].write(row[1])
-        cols[2].write(row[2])
-        cols[3].write(row[3])
+    with tabs[0]:
+        # Form tab to insert new entries
+        st.write("To update an entry, simply fill the form again")
+        data = sp.pydantic_form(key="word_form", model=WordBank)
         
-        # cols[2].markdown(
-        #     f'<a target="_self" href="/?id=123" style="display: inline-block; padding: 6px 10px; background-color: #4CAF50; color: white; text-align: center; text-decoration: none; font-size: 12px; border-radius: 4px;">Action Text on Button</a>',
-        #     unsafe_allow_html=True,
-        # )
+        if data:
+            insert_word(data)
+
+    with tabs[1]:
+        search_col, filter_col = st.columns(2)
+
+        # Search bar
+        search_term = search_col.text_input("🔎 Search for a word:", key="search_term")
+
+        # Filter by progress
+        selected_progress = filter_col.selectbox("Filter by Progress:", ["All"] + [progress.value for progress in ProgressValue], key="selected_progress")
+
+        # Fetch data based on search and filter
+        data = cur.execute(
+            """
+            SELECT * FROM word_list
+            WHERE (word LIKE ? OR definition LIKE ?) AND (? = 'All' OR progress = ?)
+            """,
+            (f"%{search_term}%", f"%{search_term}%", selected_progress, selected_progress),
+        ).fetchall()
+
+        cols = st.columns(5)
+        cols[0].write("")
+        cols[1].write("Progress")
+        cols[2].write("Word")
+        cols[3].write("POS")
+        cols[4].write("Definition")
+
+        def get_progress_color(progress):
+            if progress == "unfamiliar":
+                return "#F76F65"
+            elif progress == "recognized":
+                return "#F3A738"
+            elif progress == "understood":
+                return "#FDF984"
+            elif progress == "mastered":
+                return "#94C747"
+            else:
+                return "black"
+        
+        for row in data:
+            progress_color = get_progress_color(row[4])
+            cols = st.columns(5)
+
+            delete_button_id = f"delete_button_{row[0]}"
+            if cols[0].button("Delete", key=delete_button_id):
+                # Button clicked, delete the entry
+                delete_entry(row[0])
+                st.rerun()
+
+            cols[1].write(f'<div style="display: inline-block; padding: 6px 10px; background-color:{progress_color}; border-radius: 10px;">{row[4]}</div>', unsafe_allow_html=True)
+            cols[2].write(row[1])
+            cols[3].write(row[2])
+            cols[4].write(row[3])
 
 main()
